2830ed5a0c8174608cc5bf77e9bfceac019865c3
Victoria <victoria.rezbastos@gmail.com>
2021-12-11 17:31:39 -0300

    Reestruturando app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
-const doacaoRouter = require('./routes/doacaoRoutes');
-const hospedagemRouter = require('./routes/hospedagemRoutes');
-
 require('dotenv-safe').config();
 const db = require('./database/MongoConfig');
-
+const cors = require('cors');
 const express = require('express');
+
 const app = express();
 
-const cors = require('cors');
-app.use(cors());
-
-app.use(express.json());
-app.use('/doacao', doacaoRouter);
-app.use('/hospedagem', hospedagemRouter)
+const doacaoRouter = require('./routes/doacaoRoutes');
+const hospedagemRouter = require('./routes/hospedagemRoutes');
+const authRouter = require('./routes/authRoutes');
+const index = require('./routes/index');
 
 db.connect();
+
+app.use(cors());
+app.use(express.json());
+app.use('/doacao', doacaoRouter);
+app.use('/hospedagem', hospedagemRouter);
+app.use('/user', authRouter);
+app.use('/', index);
 
 module.exports = app;
