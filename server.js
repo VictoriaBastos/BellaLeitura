9f33ac6464f4c25f9c2330c285867d8144a05bb9
Victoria <victoria.rezbastos@gmail.com>
2021-12-11 10:51:37 -0300

    Adicionando variável de ambiente para PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const app = require('./src/app')
-const PORT = process.env.PORT || 8080
+const app = require('./src/app');
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`)
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 })
