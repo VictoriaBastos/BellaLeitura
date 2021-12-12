049486cb172e7517a9e83d8cb3aef4f34289356e
Victoria <victoria.rezbastos@gmail.com>
2021-12-12 14:15:57 -0300

    middleware

--- src/routes/doacaoRoutes.js
+++ src/routes/doacaoRoutes.js
@@ -1,8 +1,10 @@
 const doacaoController = require('../controller/doacaoController')
-
 const express = require("express")
+const authMiddleware = require('../middleware/auth')
 const router = express.Router()
 
+
+router.use(authMiddleware);
 router.get('/', doacaoController.getAll)
 router.post('/', doacaoController.create)
 
