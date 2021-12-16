a70e91c81bbf37395a005811363381dbcef7d60b
Victoria <victoria.rezbastos@gmail.com>
2021-12-15 21:42:46 -0300

    Fixing Routes

--- src/routes/hospedagemRoutes.js
+++ src/routes/hospedagemRoutes.js
@@ -5,10 +5,10 @@ const express = require("express")
 const router = express.Router()
 
 //Public Routes
-//router.get('/', hospedagemController.getAll)
-//router.get('/', hospedagemController.getByNeighborhood)
-//router.get('/', hospedagemController.getByCity)
-//router.get('/', hospedagemController.getByState)
+router.get('/', hospedagemController.getAll)
+router.get('/', hospedagemController.getByNeighborhood)
+router.get('/', hospedagemController.getByCity)
+router.get('/', hospedagemController.getByState)
 
 
 //Private Routes
@@ -16,8 +16,9 @@ const router = express.Router()
 //router.put('/:id', hospedagemController.update)
 //router.delete('/id', hospedagemController.delete)
 
-//router.post('/', checkToken,  hospedagemController.create)
-//router.put('/:id', checkToken, hospedagemController.update)
-//router.delete('/:id', checkToken,  hospedagemController.delete)
+//Private Routes
+router.post('/', checkToken,  hospedagemController.create)
+router.put('/:id', checkToken, hospedagemController.update)
+router.delete('/:id', checkToken,  hospedagemController.delete)
 
 module.exports = router;
--- src/routes/livroRoutes.js
+++ src/routes/livroRoutes.js
@@ -5,15 +5,15 @@ const express = require("express")
 const router = express.Router()
 
 //Public Routes
-//router.get('/', livrosController.getAll)
-//router.get('/', livrosController.getByGenre)
-//router.get('/', livrosController.getByTitle)
-//router.get('/', livrosController.getByAuthor)
+router.get('/', livrosController.getAll)
+router.get('/', livrosController.getByGenre)
+router.get('/', livrosController.getByTitle)
+router.get('/', livrosController.getByAuthor)
 
 
 //Private Routes
-//router.post('/', checkToken,  livrosController.create)
-//router.put('/', checkToken, livrosController.update)
-//router.delete('/', checkToken,  livrosController.delete)
+router.post('/', checkToken,  livrosController.create)
+router.put('/', checkToken, livrosController.update)
+router.delete('/', checkToken,  livrosController.delete)
 
 module.exports = router;
